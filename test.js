3b57fa2f1fea51fc1b7b986d77618472d718f42a
Leonardo Velarde <lvelarde@fintonic.com>
2020-01-23 17:07:02 -0600

    Test

--- test.js
+++ test.js
@@ -9,3 +9,7 @@ test("Adding 1 + 1 equals 2", () => {
 test("Adding 2 + 2 equals 4", () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+test("Adding 3 + 3 equals 6", () => {
+  expect(sum(3, 3)).toBe(6);
+});
